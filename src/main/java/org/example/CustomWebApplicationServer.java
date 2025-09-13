142114dc662166c40640de11a513b9df3591a4b2
kme-developer <electruc0095@gmail.com>
2025-09-13 18:03:59 +0900

    feat. fix to use thread pool.

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
   private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
   private final int port;
+  private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
   public CustomWebApplicationServer(int port) {
     this.port = port;
@@ -25,7 +28,7 @@ public class CustomWebApplicationServer {
       while ((clientSocket = serverSocket.accept()) != null) {
         logger.info("[Custom Web Application Server] client connected.");
 
-        new Thread(new ClientRequestHandler(clientSocket)).start();
+        executorService.execute(new ClientRequestHandler(clientSocket));
       }
     }
   }
